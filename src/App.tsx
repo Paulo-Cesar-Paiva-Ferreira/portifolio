b49eafc4ea4ee9f306060d468d2222e54f099059
Paulo César <pcgms7@hotmail.com>
2023-03-01 22:03:33 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Teste from './texte'
+import EstiloGlobal from './styled'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App

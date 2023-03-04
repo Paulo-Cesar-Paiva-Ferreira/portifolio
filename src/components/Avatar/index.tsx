e82f7058398ccebc27ba796641f96f758f958fdb
Paulo César <pcgms7@hotmail.com>
2023-03-03 21:42:47 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/paulo-cesar-paiva-ferreira.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/paulo-cesar-paiva-ferreira.png" />
-    <Titulo fontSize={20}>Paulo César Paiva Ferreira</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Paulo César Paiva Ferreira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        paulo-cesar-paiva-ferreira
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Full Stack Python
+      </Descricao>
+      <BotaoTema>TRocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
